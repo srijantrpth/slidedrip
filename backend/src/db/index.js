ed4d87ce3373e4d024a7d2f488cef1f172b38576
Srijan Tripathi <srijantrpth@gmail.com>
2025-02-19 20:58:10 +0530

    Enhance backend setup with improved MongoDB connection handling and package configuration

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,9 +1,15 @@
-import mongoose from 'mongoose';
-import {DB_NAME} from "../constants.js"
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
-try {
+  try {
     
-} catch (error) {
-    
-}
-}
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`
+    );
+    console.log(`MongoDB Connected || DB Host ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDB Connection Error \n`, error);
+    throw error;
+  }
+};
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,18 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
-import {app} from "./app.js"
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB Connection Failed: ${err}`);
+    process.exit(1);
+  });
