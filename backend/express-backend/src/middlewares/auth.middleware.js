e44b97585ce80c96b647e6bf87726ca2946ad15c
Srijan Tripathi <srijantrpth@gmail.com>
2025-02-27 22:00:10 +0530

    Enhance content generation controller with user authentication checks and improved error handling for file uploads

--- backend/express-backend/src/middlewares/auth.middleware.js
+++ backend/express-backend/src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
-      throw new ApiError("Unauthorized Request");
+      throw new ApiError("Unauthorized Request, Please login to Continue!");
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select(
