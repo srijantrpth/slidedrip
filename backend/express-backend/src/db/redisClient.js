f5591721d1ef064bbdbadca899d1954765b3530b
Srijan Tripathi <srijantrpth@gmail.com>
2025-03-02 19:35:32 +0530

    Initialize frontend and backend projects with Vite and Express, add basic configurations and dependencies

--- backend/express-backend/src/db/redisClient.js
+++ backend/express-backend/src/db/redisClient.js
@@ -2,14 +2,13 @@ import Redis from 'ioredis';
 
 const redis = new Redis({
     port: process.env.REDIS_PORT || 6379,
-    host: 'redis' || 'localhost',
+    host: process.env.REDIS_HOST || 'localhost',
     maxRetriesPerRequest: 3,
     retryStrategy: function(times){
         const delay = Math.min(times*100, 5000);
         console.log(`Redis Retry Strategy: Attempt ${times}, Retrying in ${delay}ms`);
         return delay;
-        
     }
-})
+});
 
 export default redis;
